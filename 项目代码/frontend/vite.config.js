74b21498fc24913286226cc2c705c987c5d32493
D-ay-S <984651554@qq.com>
2026-04-08 11:20:52 +0800

    基础配置完成

--- 项目代码/frontend/vite.config.js
+++ 项目代码/frontend/vite.config.js
@@ -6,6 +6,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
